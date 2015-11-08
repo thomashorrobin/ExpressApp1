baa3e4b031c96f987603cf50da261f980829fb5f
Thomas Horrobin <thomas@getsignal.info>
2015-11-08 14:46:43 +1300

    initial commit

---
+++ ExpressApp1/controllers/peopleController.js
@@ -0,0 +1,30 @@
+﻿(function (peopleController) {
+    
+    var people = [
+        { id: 1, name: 'Thomas Horrobin', age: 25 },
+        { id: 2, name: 'William Fussey', age: 31 },
+        { id: 3, name: 'Blake Crayton-Brown', age: 25 }
+    ];
+
+
+    peopleController.init = function (app) {
+        console.log('got here');
+
+        app.get("/api/people", function (req, res) { 
+            res.set("Content-Type", "application/json");
+            res.send(people);
+        });
+        
+        app.get("/api/people/:id", function (req, res) {
+            console.log(req.params.id);
+            res.set("Content-Type", "application/json");
+            people.forEach(function (element, index) {
+                if (element.id == req.params.id) {
+                    res.send(element);
+                }
+            });
+            //res.send('error');
+        });
+    };
+
+})(module.exports);
---
+++ ExpressApp1/routes/index.js
@@ -0,0 +1,10 @@
+﻿var express = require('express');
+var mongoose = require('mongoose');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function (req, res) {
+    res.render('index', { title: 'Express', info: 'This is some info about this page' });
+});
+
+module.exports = router;
